4865c1a6f41a1ed5d2ceb52e3ba78468563706bf
Luan <Luan.staner@gmail.com>
2019-06-08 08:28:56 -0300

    Aumentar largura da caixa form em contatos

--- sitePessoalApp/static/js/scriptContato.js
+++
@@ -1,7 +0,0 @@
-const btnEnviar = document.querySelector("#btnEnviar");
-btnEnviar.addEventListener("click", function(){
-btnEnviar.classList.remove("btn-primary");
-btnEnviar.classList.add("btn-success");
-btnEnviar.textContent = "Enviado com sucesso !";
-
-  });
