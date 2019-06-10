6b6b480b03ff1b6fe0c1e8514520da5d42ed3216
Luan <Luan.staner@gmail.com>
2019-06-10 15:35:44 -0300

    Desativação da função de debug do django

--- projetoMeuPessoal/settings.py
+++ projetoMeuPessoal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yvki)!@=h4l2j=0(maj_slht9coqq2e+2ou+y7m#_jwmbf)+jj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
